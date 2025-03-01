3080d392b453f0bc780b1987a775178b3e269f65
0x1args <stylexload@gmail.com>
2025-03-01 17:20:16 +0200

    Added MoveWindowCommand, MinimizeWindowCommand, MaximizeWindowCommand

--- WpfApp2/MVVM/ViewModel/MainViewModel.cs
+++ WpfApp2/MVVM/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using System.Windows;
+using System.Windows.Input;
 using WpfApp2.Core;
 using WpfApp2.Interfaces;
 
@@ -20,17 +21,34 @@ public class MainViewModel : Core.ViewModel
 
     public ICommand NavigateToHomeViewCommand { get; set; }
     public ICommand NavigateToSettingsViewCommand { get; set; }
+    public ICommand MoveWindowCommand { get; set; }
+    public ICommand MinimizeWindowCommand { get; set; }
+    public ICommand MaximizeWindowCommand { get; set; }
 
     public MainViewModel(
         INavigationService navigationService)
     {
         _navigationService = navigationService;
 
-        NavigateToHomeViewCommand = new RelayCommand(
-            _ => NavigationService.NavigationTo<HomeViewModel>());
+        NavigateToHomeViewCommand = new RelayCommand(_ =>
+            NavigationService.NavigationTo<HomeViewModel>());
 
-        NavigateToSettingsViewCommand = new RelayCommand(
-            _ => NavigationService.NavigationTo<SettingsViewModel>());
+        NavigateToSettingsViewCommand = new RelayCommand(_ =>
+            NavigationService.NavigationTo<SettingsViewModel>());
 
+        MoveWindowCommand = new RelayCommand(_ =>
+            Application.Current.MainWindow!.DragMove());
+
+        MinimizeWindowCommand = new RelayCommand(_ =>
+            Application.Current.MainWindow!.WindowState = WindowState.Minimized);
+
+        MaximizeWindowCommand = new RelayCommand(_ =>
+        {
+            var state = Application.Current.MainWindow!.WindowState;
+
+            state = state == WindowState.Maximized 
+                ? WindowState.Normal 
+                : WindowState.Maximized;
+        });
     }
 }
